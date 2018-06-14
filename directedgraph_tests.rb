d254ea4e02d973d45ce4bcca4c9db1e7fbb8a049
Grant Runyon <runyon_gc@newriver.edu>
2018-06-13 20:13:44 -0400

    Added first cost test, passing.

--- directedgraph_tests.rb
+++ directedgraph_tests.rb
@@ -11,14 +11,21 @@ class DirectedGraphTest < Minitest::Test
     assert_equal(1,1)
   end
 
-  def test_route_one_path
+  def test_route_one_shortest_path
     path = ["A","B","C"]
     assert_equal(path, @test.find_shortest_path("A","C"))
   end
 
-  def test_route_two_path
+  def test_route_two_shortest_path
     path = ["A", "D"]
     assert_equal(path, @test.find_shortest_path("A","D"))
   end
+
+  def test_route_one_cost
+    path1 = ["A","B","C"]
+    path = @test.find_path_cost(path1)
+    assert_equal(path, 9)
+  end
+
 
 end
